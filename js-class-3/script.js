72c148183272600a636288a7e017e66a7953e505
Paru Maharjan <parumaharjan098@gmail.com>
2026-03-22 12:00:24 +0545

    iteration

--- js-class-3/script.js
+++ js-class-3/script.js
@@ -1,3 +1,15 @@
+/*
 for (let i = 1; i <= 10; i++) {
     console.log(`2 x ${i} = ${2 * i}`);
+}
+*/
+
+for (let i = 1; i <= 20; i++) {
+  if ((i == 3)) {
+    continue;
+  }
+  if (i >= 15) {
+    break;
+  }
+  console.log(i);
 }
